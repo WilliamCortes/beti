27db70aeea1c2e7bc7d42518a43da011fc2a885d
William Cortes <williamcortesro@outlook.es>
2021-09-29 18:12:11 -0500

    agrego el progreso en numeros

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,18 +1,31 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import '../styles/progress.css'
 
-export const Progress = ({done}) => {
+export const Progress = ({ done }) => {
 
-	const style = {
-		opacity: 1,
-		width: `${done}%`
-	}
-	
-	return (
-		<div className="progress">
-			<div className="progress-done" style={style}>
-				
-			</div>
-		</div>
-	)
+    const style = {
+        opacity: 1,
+        width: `${done}%`
+    }
+
+    return (
+        <div className="progress">
+            <div className="progress_done" style={style}>
+            </div>
+            <div className='progress_line'>
+                <span>|</span>
+                <span>|</span>
+                <span>|</span>
+                <span>|</span>
+                <span>|</span>
+            </div>
+            <div className='progress_number'>
+                <span>1</span>
+                <span>2</span>
+                <span>3</span>
+                <span>4</span>
+                <span>5</span>
+            </div>
+        </div>
+    )
 }
